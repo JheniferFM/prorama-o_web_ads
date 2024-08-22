c9ddd2828d8e64764a0a78f3e9dfb6636a417dc8
Jhenifer <jhenifer.meneses@sempreceub.com>
2024-08-22 10:30:42 -0300

    abc

---
+++ index.js
@@ -0,0 +1,16 @@
+let pagina = document;
+let botaoSuberter = document.getElementById("botao-submeter");
+botaoSuberter.addEventListener("click" , submeter);
+
+let nome = document.getElementByIdId("nome").value;
+console.log(nome);
+
+function submeter(){
+    let nome = document.getElementById("nome").value;
+    console.log(nome);
+    if(nome == " ")
+    alert("preencha o campo nome");
+    let telefone = document.getElementById("telefone").value;
+    let email= document.getElementById("email").value;
+    let datanascimneto=document.getElementById("data-nascimeno").value;
+}
